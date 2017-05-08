b13d7a575296e12cb6a1a0b3e09e9bb3e8ab183b
Jon Tamir <jtamir@eecs.berkeley.edu>
2017-05-08 14:40:21 -0700

    robustify jt3d image grabber

--- images/run.sh
+++ images/run.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-time=60 # seconds
+interval=60 # seconds
+delay=1440 # frames
 
 script=./jt3dp-webcam.sh
 
+dir="../jt3dp-webcam-images"
+im="jt3dp-webcam"
+
+idx=0
+
 while true
 do
-$script
-sleep $time
+
+	im_new="${im}-${idx}.jpg"
+
+	cp ${dir}/${im_new} ${im}.jpg
+	chmod o+x $im.jpg
+
+	$script ${dir}/$im_new
+
+	idx=$((${idx} + 1))
+
+	if [[ "${idx}" -eq "${delay}" ]] ; then
+		idx=0
+	fi
+
+	sleep $interval
 done
